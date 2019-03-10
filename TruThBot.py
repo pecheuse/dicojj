f6f30e3ac187e093bcae2e42f98a141a67c7871b
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-10 14:24:55 +0900

    Update TruThBot.py

--- TruThBot.py
+++ TruThBot.py
@@ -97,7 +97,7 @@ async def on_message(message):
         Z9 = Z8.replace('9', ':nine:')
         embed = discord.Embed(description=Z9, color=0x383b38)
     if message.content.startswith("T명령어"):
-        await bot.send_message(message.channel, "봇 <내용> (대화 기능)\ntr <내용> (내용 강조)\n검색 <내용> (이미지 검색)\n비밀 <내용> (비밀 메시지)\nT표현 <내용> (이모티콘으로 영어,숫자 표시)
+        await bot.send_message(message.channel, "봇 <내용> (대화 기능)\ntr <내용> (내용 강조)\n검색 <내용> (이미지 검색)\n비밀 <내용> (비밀 메시지)\nT표현 <내용> (이모티콘으로 영어,숫자표시))")
         
         await bot.send_message(message.channel, embed=embed)
 access_token = os.environ["BOT_TOKEN"]
