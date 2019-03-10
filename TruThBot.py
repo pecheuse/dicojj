b397a648d429811876fc0ef8e109cbdc3b96798f
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-10 15:42:43 +0900

    Update TruThBot.py

--- TruThBot.py
+++ TruThBot.py
@@ -101,26 +101,12 @@ async def on_message(message):
         embed = discord.Embed(description= "봇 <내용> (대화 기능)\ntr <내용> (내용 강조)\n검색 <내용> (이미지 검색)\n비밀 <내용> (비밀 메시지)\nT표현 <내용> (이모티콘으로 영어,숫자표시)", color=0x383b38)
         await bot.send_message(message.channel, embed=embed)
     if message.content.startswith("T투표"):
-        async def quickpoll(self, ctx, question, *options: str):
-            if len(options) <= 1:
-                await self.bot.say('You need more than one option to make a poll!')
-                return
-            if len(options) > 10:
-                await self.bot.say('You cannot make a poll for more than 10 things!')
-                return
-
-            if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
-                reactions = ['✅', '❌']
-            else:
-                reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
-            description = []
-            for x, option in enumerate(options):
-                description += '\n {} {}'.format(reactions[x], option)
-            embed = discord.Embed(title=question, description=''.join(description))
-            react_message = await self.bot.say(embed=embed)
-            for reaction in reactions[:len(options)]:
-                await self.bot.add_reaction(react_message, reaction)
-            embed.set_footer(text='Poll ID: {}'.format(react_message.id))
-            await self.bot.edit_message(react_message, embed=embed)
+        vote = message.content[4:].split("/")
+        await bot.send_message(message.channel, "투표" +vote[0])
+        for TTT in range(1, len(vote)):
+            choose = await bot.send_message(message.channel, "```"+vote[TTT])
+            await bot.add_reaction(choose, '✅')
+            await bot.add_reaction(choose, '❌')
+        
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
