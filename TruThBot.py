d75f9a24076d5c9ddd8972a4881f72563965b71a
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-10 15:33:49 +0900

    Update TruThBot.py

--- TruThBot.py
+++ TruThBot.py
@@ -98,7 +98,7 @@ async def on_message(message):
         embed = discord.Embed(description=Z9, color=0x383b38)
         await bot.send_message(message.channel, embed=embed)
     if message.content.startswith("T명령어"):
-        embed = bot.send_message(message.channel, "봇 <내용> (대화 기능)\ntr <내용> (내용 강조)\n검색 <내용> (이미지 검색)\n비밀 <내용> (비밀 메시지)\nT표현 <내용> (이모티콘으로 영어,숫자표시)")
+        embed = discord.Embed(description= "봇 <내용> (대화 기능)\ntr <내용> (내용 강조)\n검색 <내용> (이미지 검색)\n비밀 <내용> (비밀 메시지)\nT표현 <내용> (이모티콘으로 영어,숫자표시)", color=0x383b38)
         await bot.send_message(message.channel, embed=embed)
     if message.content.startswith("T투표"):
         async def quickpoll(self, ctx, question, *options: str):
