cdab002d274b5b14c2c9e1a057d5327a8319fddb
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2019-03-15 15:55:59 +0900

    Update TruThBot.py

--- TruThBot.py
+++ TruThBot.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import asyncio
 import requests
-import random
 from bs4 import BeautifulSoup as bs
 client = discord.Client()
 bot = commands.Bot(command_prefix='T')
