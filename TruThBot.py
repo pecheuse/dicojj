1511a71633ec1981e679baba0f9804e0922101cc
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2021-03-28 16:07:25 +0900

    Update TruThBot.py

--- TruThBot.py
+++ TruThBot.py
@@ -153,9 +153,12 @@ async def on_message(message):
         for i in range(split6):
             if i <= 10:
                 await message.channel.send(split7)
-    if message.author.id == "669851548414640131":
+    if message.author.id == 669851548414640131:
+        embed = discord.Embed(title="o3983은 발언권 없어짐", description=message.content,
+                              color=0x383b38)
+        embed.set_thumbnail(url=message.author.avatar_url)
         await message.channel.purge(limit=1)
-        await message.channel.send("o3983은 발언권이 없어짐", message.content)
+        await message.channel.send(embed = embed)
 
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
